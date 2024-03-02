414ba20ea7d51277b7914fd341fccc884e4ed825
HactuallyBenji <mrbenamiller@gmail.com>
2024-03-02 11:53:18 -0800

    Create separate scripts for aliase and bash prompt set up

---
+++ aliases-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Remove existing symlinks
+rm -rf ~/.aliases
+
+ALIASES_SOURCE="source ~/.aliases"
+BASHRC="$HOME/.bashrc"
+
+append_if_not_exist() {
+  local line="$1"
+  local file="$2"
+  grep -qF -- "$line" "$file" || echo "$line" >> "$file"
+}
+
+# Append source lines if they don't exist
+append_if_not_exist "$ALIASES_SOURCE" "$BASHRC"
+
+echo "Updated .bashrc successfully with aliases."
