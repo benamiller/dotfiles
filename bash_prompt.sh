094275a7d36f196701a7c95c5428d3962df13319
HactuallyBenji <mrbenamiller@gmail.com>
2024-03-02 17:11:31 -0800

    Add last bit of color for git

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -4,7 +4,8 @@ export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
 PS1="\[\e[97;45m\] \u \[\e[m\]\[\e[44;35m\]\[\e[m\]"    # username
-PS1+="\[\e[44;92m\] \w \[\e[m\]\[\e[34m\]"    # working directory
-PS1+="\[\e[0;95m\]\$(git branch 2>/dev/null | grep '^*' | colrm 1 1)\[\e[m\]"    # git branch
+PS1+="\[\e[44;97m\] \w \[\e[m\]\[\e[42;34m\]\[\e[m\]"    # working directory
+PS1+="\[\e[42;97m\]\$(git branch 2>/dev/null | grep '^*' | colrm 1 1)"
+PS1+=" \[\e[m\]\[\e[32m\]\[\e[m\]"    # git branch
 PS1+="  "
 export PS1
