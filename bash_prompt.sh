dd43d6434384e7359fb41c772b35ba3abc9c3d50
HactuallyBenji <mrbenamiller@gmail.com>
2024-03-02 17:32:09 -0800

    Lighter colours pls

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -3,9 +3,9 @@
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
-PS1="\[\e[97;45m\] \u \[\e[m\]\[\e[44;35m\]\[\e[m\]"    # username
-PS1+="\[\e[44;97m\] \w \[\e[m\]\[\e[42;34m\]\[\e[m\]"    # working directory
-PS1+="\[\e[42;97m\]\$(git branch 2>/dev/null | grep '^*' | colrm 1 1)"
-PS1+=" \[\e[m\]\[\e[32m\]\[\e[m\]"    # git branch
+PS1="\[\e[30;105m\] \u \[\e[m\]\[\e[104;95m\]\[\e[m\]"    # username
+PS1+="\[\e[104;30m\] \w \[\e[m\]\[\e[102;94m\]\[\e[m\]"    # working directory
+PS1+="\[\e[102;30m\]\$(git branch 2>/dev/null | grep '^*' | colrm 1 1)"
+PS1+=" \[\e[m\]\[\e[92m\]\[\e[m\]"    # git branch
 PS1+="  "
 export PS1
