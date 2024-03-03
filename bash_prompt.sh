0373f85d8f8d58060ddd474e1ca510c4997c3d2b
HactuallyBenji <mrbenamiller@gmail.com>
2024-03-02 17:02:41 -0800

    Adding some arrows, and ANSI colors for now

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -3,9 +3,8 @@
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
-PS1="\[\e[0;93m\]\u\[\e[m\]"    # username
-PS1+=" "
-PS1+="\[\e[0;92m\]\w\[\e[m\]"    # working directory
+PS1="\[\e[97;45m\] \u \[\e[m\]\[\e[44;35m\]\[\e[m\]"    # username
+PS1+="\[\e[44;92m\] \w \[\e[m\]\[\e[34m\]"    # working directory
 PS1+="\[\e[0;95m\]\$(git branch 2>/dev/null | grep '^*' | colrm 1 1)\[\e[m\]"    # git branch
 PS1+="  "
 export PS1
