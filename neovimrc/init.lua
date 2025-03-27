60b9dcb6915a1fbc702b3f802d9c5ecec85a32ac
benamiller <mrbenamiller@gmail.com>
2025-03-27 11:11:04 +0000

    Adds markdown preview for the vibes

--- neovimrc/init.lua
+++ neovimrc/init.lua
@@ -251,6 +251,12 @@ require("lazy").setup({
 	--    require('gitsigns').setup({ ... })
 	--
 	-- See `:help gitsigns` to understand what the configuration keys do
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = "cd app && npm install",
+	},
 
 	{
 		"stevearc/oil.nvim",
